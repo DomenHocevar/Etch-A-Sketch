ba2a724ce1b91d8754c898e2b307948441a262e2
domen <domenhocevar1@gmail.com>
2021-06-23 17:50:05 +0200

    Added input graphics and implemented working grid size input

--- script.js
+++ script.js
@@ -2,27 +2,37 @@
 
 
 
+function mapSizeSubmission(params) {
+    const textInput = document.querySelector("#mapSizeInput");
+    const value = parseInt(textInput.value);
+    if (!(parseInt(textInput.min) <= value && value <= parseInt(textInput.max)))
+    {
+        alert("Enter a valid number!");
+        return;
+    }
+    mapInit(value);
+}
 
 
-
-function init(size) {
+function mapInit(size) {
     const map = document.querySelector("#map");
 
     const children = Array.from(map.childNodes);
     children.forEach(child => map.removeChild(child));
 
-    console.log("1fr".repeat(size));
     map.style.gridTemplateColumns = "1fr ".repeat(size);
     for (i = 0; i < size; i++)
     {
         for (j = 0; j < size; j++)
         {
-            console.log(i + " " + j);
             const block = document.createElement("div");
             block.classList.add("block");
+            
             map.appendChild(block);
         }
     }
 }
 
-init(100);
+
+
+mapInit(10);
